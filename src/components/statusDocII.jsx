8e156abf4cef447fbf93c2ff3f0f406884cc11ed
W.M.P.I.Dharmagunawardana <it21132346@my.sliit.lk>
2024-10-25 00:49:29 +0530

    push

--- src/components/statusDocII.jsx
+++ src/components/statusDocII.jsx
@@ -1,25 +1,16 @@
-import React from "react";
+import React from 'react'
 import { FaDownload } from "react-icons/fa6";
 
-export const Documents =(props)=>{
-    return(
-        <div id="documents" className="text-center">
+export const StatusDocII = (props) => {
+    return (
+        <div id="statusDocII" className="text-center">
             <div className="container">
                 <div className="section-title">
-                    <h2>Documents</h2>
+                    {/* <h2>Documents</h2> */}
                 </div>
-
-{/* ************************** */}
-
-      {/* <div class="card">
-  <p>A glass-like card to demonstrate the <strong>Glassmorphism UI design</strong> trend.</p>
-  <p class="card-footer">Created by Rahul C.</p>
-</div> */}
-
-{/* ******************************* */}
                 <div className="row">
-                    <h4>Presentation Documents</h4>
-                    <div className="portfolio-items">
+                    <h4>Status Document II</h4>
+                    <div>
                         {props.data
                             ? props.data.map((d, i) => (
                                 <div
